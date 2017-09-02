7eadf194a59d51bf218766546b25f8b08f0a3770
Olga <kochepasovaolga@gmail.com>
2017-09-02 13:37:19 -0400

    Update files to meet the 2nd review suggestions.

--- tournament.py
+++ tournament.py
@@ -1,5 +1,5 @@
 #!/usr/bin/env python
-# 
+#
 # tournament.py -- implementation of a Swiss-system tournament
 #
 
@@ -8,9 +8,12 @@ import psycopg2
 
 def connect():
     """Connect to the PostgreSQL database.  Returns a database connection."""
-    db = psycopg2.connect("dbname=tournament")
-    cursor = db.cursor()
-    return db, cursor
+    try:
+        db = psycopg2.connect("dbname=tournament")
+        cursor = db.cursor()
+        return db, cursor
+    except:
+        print("Could not connect to the PostgreSQL database.")
 
 
 def deleteMatches():
@@ -50,19 +53,19 @@ def countPlayers():
     # Body
     query = "SELECT count(*) as num FROM players;"
     c.execute(query)
-    num = c.fetchall()
+    num = c.fetchone()
 
     # Closing
     db.close()
-    return int(num[0][0])
+    return int(num[0])
 
 
 def registerPlayer(name):
     """Adds a player to the tournament database.
-  
+
     The database assigns a unique serial id number for the player.  (This
     should be handled by your SQL database schema, not in your Python code.)
-  
+
     Args:
       name: the player's full name (need not be unique).
     """
@@ -82,8 +85,8 @@ def registerPlayer(name):
 def playerStandings():
     """Returns a list of the players and their win records, sorted by wins.
 
-    The first entry in the list should be the player in first place, or a player
-    tied for first place if there is currently a tie.
+    The first entry in the list should be the player in first place, or a
+    player tied for first place if there is currently a tie.
 
     Returns:
       A list of tuples, each of which contains (id, name, wins, matches):
@@ -99,12 +102,12 @@ def playerStandings():
     query = "SELECT * FROM standings;"
     c.execute(query)
 
-    list = [(int(row[0]), str(row[1]), int(row[2]), int(row[3])) 
-             for row in c.fetchall()]
+    list = [(int(row[0]), str(row[1]), int(row[2]), int(row[3]))
+            for row in c.fetchall()]
 
     # Closing
     db.close()
-    return list;
+    return list
 
 
 def reportMatch(winner, loser):
@@ -121,21 +124,21 @@ def reportMatch(winner, loser):
     if str(winner).isdigit() and str(loser).isdigit():
         query = "INSERT INTO matches VALUES (%s);"
         param = winner+', '+loser
-        c.execute(query, param))
+        c.execute(query, param)
         db.commit()
 
     # Closing
     db.close()
- 
- 
+
+
 def swissPairings():
     """Returns a list of pairs of players for the next round of a match.
-  
+
     Assuming that there are an even number of players registered, each player
     appears exactly once in the pairings.  Each player is paired with another
     player with an equal or nearly-equal win record, that is, a player adjacent
     to him or her in the standings.
-  
+
     Returns:
       A list of tuples, each of which contains (id1, name1, id2, name2)
         id1: the first player's unique id
@@ -151,7 +154,7 @@ def swissPairings():
 
     # Body
     list = [(record[i][0], record[i][1], record[i+1][0], record[i+1][1])
-             for i in range(0, len(record), 2)]
+            for i in range(0, len(record), 2)]
 
     # Closing
     db.close()
--- tournament.sql
+++ tournament.sql
@@ -9,9 +9,6 @@
 -- You can write comments in this file by starting them with two dashes, like
 -- these lines here.
 DROP DATABASE IF EXISTS tournament;
-DROP TABLE IF EXISTS players CASCADE;
-DROP TABLE IF EXISTS matches CASCADE;
-
 CREATE DATABASE tournament;
 \c tournament
 
